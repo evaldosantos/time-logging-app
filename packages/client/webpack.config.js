879cbcde110b0134c757db988b180e710e960a3e
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-20 20:00:15 -0300

    add webpack to client setup

---
+++ packages/client/webpack.config.js
@@ -0,0 +1,8 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src', 'index.html'),
+  })]
+};
