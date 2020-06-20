49e0722e4ddc51643e80cd92a9602e5a54361526
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-20 19:27:33 -0300

    list timers

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,24 @@
-console.log("@time-logging-app hello!")
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+const app = express();
+
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/api/timers', async (req, res) => {
+  try {
+    const data = await readFile(DATA_FILE);
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  } catch(e) {
+    res.status(500);
+  }
+});
+
+app.listen(app.get('port'));
