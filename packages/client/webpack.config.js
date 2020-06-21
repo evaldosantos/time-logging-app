d6a50bf0ec7b8e62f43950e5037c9fe66ff8c522
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-21 09:46:39 -0300

    add/setup babel parse js files in babel fix webpack/babel errors

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -2,6 +2,18 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
+  entry: path.resolve(__dirname, 'src', 'app.js'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
