c25afef84bd3fe1d0740951b921b03b7997dcd41
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-21 10:21:12 -0300

    refactor fetch

--- packages/client/src/client.js
+++ packages/client/src/client.js
@@ -1,4 +1,18 @@
-export const getTimers = (callback) =>
-  fetch('http://localhost:3000/api/timers')
-    .then((response) => response.json())
-    .then(callback);
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(`HTTP Error ${response.statusText}`);
+    error.status = response.statusText;
+    error.response = response;
+    console.log(error);
+    throw error;
+  }
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+export const getTimers = (success) =>
+  fetch('http://localhost:3000/api/timers').then(checkStatus).then(parseJSON).then(success);
